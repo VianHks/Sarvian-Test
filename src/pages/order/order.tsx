e988b40be60330f08efa44994a44afb0e1241671
HaryNug <harynugroho93@gmail.com>
2023-11-20 17:32:42 +0700

    page pesanan: diproses dan selesai

--- src/pages/order/order.tsx
+++ src/pages/order/order.tsx
@@ -5,19 +5,24 @@ import { useNavigate } from 'react-router-dom';
 import {
   Avatar,
   Box,
+  Button,
   Card,
   Container,
   Grid,
   styled,
   Tab,
   Tabs,
-  Typography
+  TextField,
+  Typography,
+  useTheme
 } from '@mui/material';
 
+import { CircleFilled } from '@nxweb/icons/material';
 import type { PageComponent } from '@nxweb/react';
 
 import { useAuth } from '@hooks/use-auth';
 
+import Bakar from '@assets/images/Bakar.png';
 import MieBaso from '@assets/images/MieBaso.png';
 
 interface StyledTabProps {
@@ -76,6 +81,7 @@ const Order: PageComponent = () => {
   const navigate = useNavigate();
   const { auth } = useAuth();
 
+  const theme = useTheme();
   const [tabValue, setTabValue] = useState('Diproses');
 
   const handleTabChange = (
@@ -103,46 +109,448 @@ const Order: PageComponent = () => {
           <CustomTab label="Dibatalkan" value="Dibatalkan" />
         </CustomTabs>
       </Box>
-      <Container sx={{ paddingInline: '0.5rem', paddingTop: '1.5rem' }}>
-          <div>
-            {tabValue === 'Diproses' &&
+      <Container sx={{ paddingInline: '0.5rem' }}>
+        <div>
+          {tabValue === 'Diproses' &&
             <Card sx={{ padding: '1rem' }}>
-                <Grid container={true} sx={{ display: 'flex' }}>
-                    <Grid item={true} sx={{ textAlign: 'start' }} xs={4}>
-                        <Typography variant="caption">No. Pesanan</Typography>
-                    </Grid>
-                    <Grid item={true} sx={{ fontWeight: 'bold', textAlign: 'center' }} xs={4}>
-                        <Typography variant="caption">#FFDA21223</Typography>
-                    </Grid>
-                    <Grid item={true} sx={{ textAlign: 'end' }} xs={4}>
-                        <Typography color="primary" variant="caption">Single Order</Typography>
-                    </Grid>
+              <Grid container={true} sx={{ display: 'flex' }}>
+                <Grid item={true} sx={{ textAlign: 'start' }} xs={4}>
+                  <Typography variant="caption">No. Pesanan</Typography>
                 </Grid>
-                <Box sx={{ alignItems: 'center', display: 'flex', justifyContent: 'space-between' }}>
-                    <Box
-                      sx={{
-                        alignItems: 'center',
-                        display: 'flex',
-                        gap: '0.5rem',
-                        justifyContent: 'start'
-                      }}
-                    >
-                        <Avatar src={MieBaso} sx={{ height: '24px', width: '24px' }} />
-                        <Typography sx={{ fontWeight: 'bold' }} variant="body1">
-                        Resto Bunda Gila
-                        </Typography>
-                    </Box>
-                    <Box sx={{ textAlign: 'end' }}>
-                        <Typography variant="caption">2 Aug 2023</Typography>
-                    </Box>
+                <Grid item={true} sx={{ fontWeight: 'bold', textAlign: 'center' }} xs={4}>
+                  <Typography variant="caption">#FFDA21223</Typography>
+                </Grid>
+                <Grid item={true} sx={{ textAlign: 'end' }} xs={4}>
+                  <Typography color="primary" variant="caption">Single Order</Typography>
+                </Grid>
+              </Grid>
+              <hr />
+              <Box sx={{ alignItems: 'center', display: 'flex', justifyContent: 'space-between', marginBottom: '0.5rem' }}>
+                <Box
+                  sx={{
+                    alignItems: 'center',
+                    display: 'flex',
+                    gap: '0.5rem',
+                    justifyContent: 'start'
+                  }}
+                >
+                  <Avatar src={MieBaso} sx={{ height: '24px', width: '24px' }} />
+                  <Typography fontWeight="bold" variant="body1">
+                    Resto Bunda Gila
+                  </Typography>
                 </Box>
-                <hr />
+                <Box sx={{ textAlign: 'end' }}>
+                  <Typography variant="caption">2 Aug 2023</Typography>
+                </Box>
+              </Box>
+              <Grid
+                container={true}
+                spacing={3}
+                sx={{ marginBottom: '0.5rem' }}
+              >
+                <Grid item={true}>
+                  <img alt="Foto" src={Bakar} />
+                </Grid>
+                <Grid item={true}>
+                  <Typography
+                    fontWeight="medium"
+                    variant="body2"
+                  >
+                    Paket Ayam Bakar
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                  >
+                    Rp. 25.000
+                  </Typography>
+                </Grid>
+                <Grid
+                  item={true}
+                  sx={{
+                    textAlign: 'end'
+                  }}
+                  xs={true}
+                >
+                  <Typography
+                    fontWeight="medium"
+                    variant="caption"
+                  >
+                    2 item
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Box sx={{ marginBottom: '0.75rem' }}>
+                <TextField
+                  fullWidth={true}
+                  placeholder="Gg. Jalanin Dulu Aja No.171"
+                  size="small"
+                  variant="outlined" />
+              </Box>
+              <Grid
+                container={true}
+                spacing={3}
+                sx={{ marginBottom: '0.5rem' }}
+              >
+                <Grid item={true}>
+                  <div
+                    style={{
+                      width: '100%',
+                      height: '100%',
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center'
+                    }}
+                  >
+                    <img
+                      alt="foto"
+                      src={Bakar}
+                      style={{ maxHeight: '100%', maxWidth: '100%' }} />
+                  </div>
+                </Grid>
+                <Grid item={true}>
+                  <Typography
+                    fontWeight="medium"
+                    variant="body2"
+                  >
+                    Paket Ayam Bakar
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                  >
+                    Rp. 25.000
+                  </Typography>
+                </Grid>
+                <Grid
+                  item={true}
+                  sx={{
+                    textAlign: 'end'
+                  }}
+                  xs={true}
+                >
+                  <Typography
+                    fontWeight="medium"
+                    variant="caption"
+                  >
+                    2 item
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Box>
+                <TextField
+                  fullWidth={true}
+                  placeholder="Gg. Jalanin Dulu Aja No.171"
+                  size="small"
+                  variant="outlined" />
+              </Box>
+              <hr />
+              <Grid
+                container={true}
+                sx={{ marginBottom: '0.75rem' }}
+              >
+                <Grid item={true}>
+                  <Typography
+                    fontWeight="Bold"
+                    variant="h6"
+                  >
+                    Total Pembayaran
+                  </Typography>
+                </Grid>
+                <Grid
+                  item={true}
+                  sx={{
+                    textAlign: 'end'
+                  }}
+                  xs={true}
+                >
+                  <Typography
+                    color="primary"
+                    fontWeight="Bold"
+                    variant="h6"
+                  >
+                    Rp. 50.000
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Box>
+                <Button
+                  color="primary"
+                  fullWidth={true}
+                  size="medium"
+                  variant="contained"
+                >
+                  Lihat Pesanan
+                </Button>
+              </Box>
             </Card>}
-            {tabValue === 'Selesai' &&
-              <> TEST 2 </>}
-            {tabValue === 'Dibatalkan' &&
-              <> TEST 3 </>}
-          </div>
+          {tabValue === 'Selesai' &&
+            <Card sx={{ padding: '1rem' }}>
+              <Grid container={true} sx={{ display: 'flex' }}>
+                <Grid item={true} sx={{ textAlign: 'start' }} xs={4}>
+                  <Typography variant="caption">No. Pesanan</Typography>
+                </Grid>
+                <Grid item={true} sx={{ fontWeight: 'bold', textAlign: 'center' }} xs={4}>
+                  <Typography variant="caption">#FFDA21223</Typography>
+                </Grid>
+                <Grid item={true} sx={{ textAlign: 'end' }} xs={4}>
+                  <Typography color="primary" variant="caption">Multi-Order</Typography>
+                </Grid>
+              </Grid>
+              <hr />
+              <Box sx={{ alignItems: 'center', display: 'flex', justifyContent: 'space-between', marginBottom: '0.5rem' }}>
+                <Box
+                  sx={{
+                    alignItems: 'center',
+                    display: 'flex',
+                    gap: '0.5rem',
+                    justifyContent: 'start'
+                  }}
+                >
+                  <Avatar src={MieBaso} sx={{ height: '24px', width: '24px' }} />
+                  <Typography fontWeight="bold" variant="body1">
+                    Resto Bunda Gila
+                  </Typography>
+                </Box>
+                <Box sx={{ textAlign: 'end' }}>
+                  <Typography variant="caption">2 Aug 2023</Typography>
+                </Box>
+              </Box>
+              <Grid
+                container={true}
+                spacing={3}
+                sx={{ marginBottom: '0.5rem' }}
+              >
+                <Grid item={true}>
+                  <img alt="Foto" src={Bakar} />
+                </Grid>
+                <Grid item={true}>
+                  <Typography
+                    fontWeight="medium"
+                    variant="body2"
+                  >
+                    Paket Ayam Bakar
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                  >
+                    Rp. 25.000
+                  </Typography>
+                </Grid>
+                <Grid
+                  item={true}
+                  sx={{
+                    textAlign: 'end'
+                  }}
+                  xs={true}
+                >
+                  <Typography
+                    fontWeight="medium"
+                    variant="caption"
+                  >
+                    2 item
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Box sx={{ marginBottom: '0.75rem' }}>
+                <TextField
+                  fullWidth={true}
+                  placeholder="Gg. Jalanin Dulu Aja No.171"
+                  size="small"
+                  variant="outlined" />
+              </Box>
+              <Grid
+                container={true}
+                spacing={3}
+                sx={{ marginBottom: '0.5rem' }}
+              >
+                <Grid item={true}>
+                  <div
+                    style={{
+                      width: '100%',
+                      height: '100%',
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center'
+                    }}
+                  >
+                    <img
+                      alt="foto"
+                      src={Bakar}
+                      style={{ maxHeight: '100%', maxWidth: '100%' }} />
+                  </div>
+                </Grid>
+                <Grid item={true}>
+                  <Typography
+                    fontWeight="medium"
+                    variant="body2"
+                  >
+                    Paket Ayam Bakar
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                  >
+                    Rp. 25.000
+                  </Typography>
+                </Grid>
+                <Grid
+                  item={true}
+                  sx={{
+                    textAlign: 'end'
+                  }}
+                  xs={true}
+                >
+                  <Typography
+                    fontWeight="medium"
+                    variant="caption"
+                  >
+                    2 item
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Box>
+                <TextField
+                  fullWidth={true}
+                  placeholder="Gg. Jalanin Dulu Aja No.171"
+                  size="small"
+                  variant="outlined" />
+              </Box>
+              <hr />
+              <Box sx={{ alignItems: 'center', display: 'flex', justifyContent: 'space-between', marginBottom: '0.5rem' }}>
+                <Box
+                  sx={{
+                    alignItems: 'center',
+                    display: 'flex',
+                    gap: '0.5rem',
+                    justifyContent: 'start'
+                  }}
+                >
+                  <Avatar src={MieBaso} sx={{ height: '24px', width: '24px' }} />
+                  <Typography fontWeight="bold" variant="body1">
+                    Resto Bunda Gila
+                  </Typography>
+                </Box>
+                <Box
+                  sx={{
+                    alignItems: 'center',
+                    display: 'flex',
+                    justifyContent: 'start'
+                  }}
+                >
+                  <CircleFilled color={theme.palette.error.main} size={4} />
+                  <Typography
+                    color="error"
+                    fontWeight="bold"
+                    variant="h6"
+                  >
+                    Gagal
+                  </Typography>
+                </Box>
+                <Box sx={{ textAlign: 'end' }}>
+                  <Typography variant="caption">2 Aug 2023</Typography>
+                </Box>
+              </Box>
+              <Grid
+                container={true}
+                spacing={3}
+                sx={{ marginBottom: '0.5rem' }}
+              >
+                <Grid item={true}>
+                  <img alt="Foto" src={Bakar} />
+                </Grid>
+                <Grid item={true}>
+                  <Typography
+                    fontWeight="medium"
+                    variant="body2"
+                  >
+                    Paket Ayam Bakar
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                  >
+                    Rp. 25.000
+                  </Typography>
+                </Grid>
+                <Grid
+                  item={true}
+                  sx={{
+                    textAlign: 'end'
+                  }}
+                  xs={true}
+                >
+                  <Typography
+                    fontWeight="medium"
+                    variant="caption"
+                  >
+                    2 item
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Box sx={{ marginBottom: '0.75rem' }}>
+                <TextField
+                  fullWidth={true}
+                  placeholder="Gg. Jalanin Dulu Aja No.171"
+                  size="small"
+                  variant="outlined" />
+              </Box>
+              <Grid
+                container={true}
+                spacing={3}
+                sx={{ marginBottom: '0.5rem' }}
+              >
+                <Grid item={true}>
+                  <div
+                    style={{
+                      width: '100%',
+                      height: '100%',
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center'
+                    }}
+                  >
+                    <img
+                      alt="foto"
+                      src={Bakar}
+                      style={{ maxHeight: '100%', maxWidth: '100%' }} />
+                  </div>
+                </Grid>
+                <Grid item={true}>
+                  <Typography
+                    fontWeight="medium"
+                    variant="body2"
+                  >
+                    Paket Ayam Bakar
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                  >
+                    Rp. 25.000
+                  </Typography>
+                </Grid>
+                <Grid
+                  item={true}
+                  sx={{
+                    textAlign: 'end'
+                  }}
+                  xs={true}
+                >
+                  <Typography
+                    fontWeight="medium"
+                    variant="caption"
+                  >
+                    2 item
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Box>
+                <TextField
+                  fullWidth={true}
+                  placeholder="Gg. Jalanin Dulu Aja No.171"
+                  size="small"
+                  variant="outlined" />
+              </Box>
+            </Card>}
+          {tabValue === 'Dibatalkan' &&
+            <> TEST 3 </>}
+        </div>
       </Container>
     </>
   );
