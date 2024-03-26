1fa8fd188ea04000adc18e7878382aae4b76a7a7
VianHks <sarvianrukie@gmail.com>
2024-03-26 15:06:08 +0700

    Delete Unused File

--- src/models/store.ts
+++ src/models/store.ts
@@ -5,67 +5,29 @@ import {
   createCommandHook, createDispatchHook, createStoreHook, createStoreProvider
 } from '@nxweb/react';
 
-import { checkoutCommand } from './checkout/commands.js';
-import { CheckoutReducer } from './checkout/reducers.js';
-import { DanaCommands } from './dana/reducers.js';
-import { FAQReducer } from './faq/reducers.js';
 import { halamanRestoCommand } from './halaman-resto/commands.js';
 import { HalamanRestoReducer } from './halaman-resto/reducers.js';
-import { homeCommand } from './home/commands.js';
-import { homeCommands, HomeReducer } from './home/reducers.js';
-import { orderCommand } from './order/commands.js';
-import { OrderReducer } from './order/reducers.js';
 import { PersonalizedRecomendationCommand, PersonalizedRecReducer, PersonalRecomendationDefault } from './personalized-recomendation/reducers.js';
-import { ProductViewReducer, ProductViewsCommand, ProductViewsDefault } from './product-view/reducers.js';
-import { productsCommand } from './products/commands.js';
-import { productsReducer } from './products/reducers.js';
-import { RatingCommand } from './rating/commands.js';
-import { RatingReducer } from './rating/reducers.js';
-import { RegistrationCommand, RegistrationDefault, RegistrationReducer } from './registration/reducers.js';
-import { UserCommand, UserDefault, UsersReducer } from './user-profile/reducers.js';
 
 import type { RootAction, RootModel } from './types.js';
-import { DEFAULT_REGISTER, RegisterCommand, RegisterReducer } from './register/reducers.js';
 
 // ** Init reducers
 const rootReducer = combineReducers({
-  beranda: HomeReducer,
-  checkout: CheckoutReducer,
-  faq: FAQReducer,
   halamanResto: HalamanRestoReducer,
-  home: HomeReducer,
-  order: OrderReducer,
-  personalizedRec: PersonalizedRecReducer,
-  products: productsReducer,
-  productView: ProductViewReducer,
-  rating: RatingReducer,
-  profile: UsersReducer,
-  register: RegisterReducer
+  personalizedRec: PersonalizedRecReducer
+
 });
 
 // ** Init models
 const rootModel: RootModel = {
-  personalizedRec: PersonalRecomendationDefault,
-  productView: ProductViewsDefault,
-  rating: {},
-  profile: UserDefault,
-  registration: RegistrationDefault
+  personalizedRec: PersonalRecomendationDefault
 };
 
 // ** Init commands
 const rootCommand = {
-  beranda: homeCommand,
-  checkout: checkoutCommand,
-  dana: DanaCommands,
   halamanResto: halamanRestoCommand,
-  home: homeCommands,
-  order: orderCommand,
-  personalizedRec: PersonalizedRecomendationCommand,
-  products: productsCommand,
-  productView: ProductViewsCommand,
-  rating: RatingCommand,
-  profile: UserCommand,
-  registration: RegistrationCommand
+  personalizedRec: PersonalizedRecomendationCommand
+
 };
 
 // ** Create store
--- src/models/types.ts
+++ src/models/types.ts
@@ -2,17 +2,8 @@
 import type { Action } from 'redux';
 import type { ThunkAction, ThunkDispatch } from 'redux-thunk';
 
-import type { CheckoutAction, CheckoutModel } from './checkout/types.js';
-import type { FAQAction, FAQModel } from './faq/types.js';
 import type { HalamanRestoAction, HalamanRestoModel } from './halaman-resto/types.js';
-import type { HomeAction, HomeModel } from './home/types.js';
-import type { OrderAction, OrderModel } from './order/types.js';
 import type { PersonalizedRecAction, PersonalRecomendationModel } from './personalized-recomendation/types.js';
-import type { ProductViewsAction, ProductViewsModel } from './product-view/types.js';
-import type { ProductsAction, ProductsModel } from './products/types.js';
-import type { RatingAction, RatingModel } from './rating/types.js';
-import type { RegisterAction, RegisterModel } from './register/types.js';
-import type { UserAction, UserModel } from './user-profile/types.js';
 
 export type SortDirection = 'asc' | 'desc';
 
@@ -35,30 +26,14 @@ export interface PagedModel<T> {
 }
 
 export interface RootModel {
-  /*
-   * Checkout?: CheckoutModel
-   * halamanResto?: HalamanRestoModel
-   */
-  home?: HomeModel
-  /*
-   * Order?: OrderModel
-   * personalizedRec?: PersonalizedRecModel
-   */
-  faq?: FAQModel
-  products?: ProductsModel
-  productView?: ProductViewsModel
+
   halamanResto?: HalamanRestoModel
-  checkout?: CheckoutModel
   personalizedRec?: PersonalRecomendationModel
-  // Beranda?: BerandaModel
-  order?: OrderModel
-  rating?: RatingModel
-  profile?: UserModel
-  registration?: UserModel
+
 }
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
-export type RootAction = CheckoutAction | FAQAction | HalamanRestoAction | HomeAction | OrderAction | PersonalizedRecAction | ProductsAction | ProductViewsAction | RatingAction | RegisterAction | UserAction | {
+export type RootAction = HalamanRestoAction | PersonalizedRecAction | {
   type: ''
 };
 export type TAction<A extends Action, R> = ThunkAction<Promise<R>, RootModel, unknown, A>;
